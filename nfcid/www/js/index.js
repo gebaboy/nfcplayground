a6d3a590ae7f5e7b45b9704ca4de0e595b4171a9
gebaboy <gebaboy@gmail.com>
2013-11-03 00:23:07 +0100

    Touch2

--- nfcid/www/js/index.js
+++ nfcid/www/js/index.js
@@ -34,6 +34,8 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        document.getElementById('touchstart').addEventListener('touchstart', hello, false);
+        document.getElementById('touchend').addEventListener('touchend', bye, false);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
